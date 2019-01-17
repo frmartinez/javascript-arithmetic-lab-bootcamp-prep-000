d97ac7edc146c9687c159020c9df7ee63d7cefe6
Fernando Martinez <martinezfer@gmail.com>
2019-01-17 21:03:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-function add(xx, yy) {
-    var xx = 1;
-    var yy = 80;
-    return add()
-}
+1 + 80 // 81
+60 - 40 // 20
+2 * 3.4 // 6.8 (there's that floating-point arithmetic again...)
+5.0 / 2.5 // 2
