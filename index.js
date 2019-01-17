7a61e53a402445cbaab0b5a399f46677dd21f723
Fernando Martinez <martinezfer@gmail.com>
2019-01-17 21:08:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function add(a, b) {
   return a + b;
 }
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divid(a, b) {
+  return a / b;
+}
